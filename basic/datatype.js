337924156cfe45bb01107ad85c46e1b3df9e5aa9
Keval Mojidra <mojidrakeval2000@gmail.com>
2024-05-16 17:06:57 +0000

    fourth

---
+++ basic/datatype.js
@@ -0,0 +1,35 @@
+"use strict";// treat all js code as newer version
+//number
+let age = 25;
+ 
+// string
+let name = 'keval';
+
+// boolean 
+let isStudent = true;
+
+//null
+let empty = null;
+
+//undefind
+let notassigned;
+
+// object
+let person={
+    name:'keval',
+    age: 25,
+    city: "ahmedabad"
+}
+
+// array
+let color = ["red","yellow","pink"]
+
+console.log("Number:" , age);
+console.log("String:" , name);
+console.log("Boolean:" , isStudent);
+console.log("Null:" , empty);
+console.log("Undefind:" , notassigned);
+console.log("Object:" , person);
+console.log("Array:" , color);
+
+
---
+++ basic/variable.js
@@ -0,0 +1,10 @@
+const accountId = 1;
+let accountEmail = 'keval@gmail.com';
+var accountPassword = '12345'
+accountCity = 'botad';
+
+accountEmail = 'kp@kp.com';
+accountPassword = '98765'
+accountCity = 'ahmedabad'
+
+console.table([accountId,accountEmail,accountPassword,accountCity ])
