9fb29cbc30f8820cb1d768380f6366d5c4994121
Keval Mojidra <mojidrakeval2000@gmail.com>
2024-05-28 18:00:01 +0000

    second

---
+++ basic/date_time.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.toDateString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.toDateString());
+
+console.log(typeof myDate);
